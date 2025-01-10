b1a3d1c8cb2a26dc1d23360926b7fd225bcb1eb3
bgunwani <bhawna.gunwani@gmail.com>
2025-01-10 14:09:41 +0530

    Second Commit

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,4 +6,4 @@ def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000)
